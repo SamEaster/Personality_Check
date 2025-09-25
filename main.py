b54a95467187435087e53dc23192f21ca2300198
Shubham <shubhamydv.578@gmail.com>
2025-09-25 15:14:53 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import numpy as np
-
-model = joblib.load('model.pkl')
-def final_model(input_data):
-    input_data = np.array(input_data).reshape(1, -1)
-
-    return model.predict(input_data)
